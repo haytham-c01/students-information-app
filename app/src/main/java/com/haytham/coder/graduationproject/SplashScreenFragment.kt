257da642e44a57b5088645f49d6133c0d9ec5140
haytham-c01 <0TU8KR21mbBs>
2020-06-10 00:23:07 +0300

    - completed sign up - completed student card version 1

---
+++ app/src/main/java/com/haytham/coder/graduationproject/SplashScreenFragment.kt
@@ -0,0 +1,36 @@
+package com.haytham.coder.graduationproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import java.util.*
+import kotlin.concurrent.schedule
+
+
+class SplashScreenFragment : Fragment() {
+
+     private val delay= 300L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        Timer().schedule(delay) {
+            //val action=
+            //findNavController().navigate()
+        }
+
+        return inflater.inflate(R.layout.fragmet_splash_screen, container, false)
+    }
+
+}
